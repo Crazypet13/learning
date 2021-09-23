011f273bff2961a2a5f252bf3b79226bf6c139ed
CrazyPet <magyar13peter13@gmail.com>
2021-09-24 01:53:20 +0200

    agghhh

--- hW2.js
+++ hW2.js
@@ -1,6 +1,19 @@
+const { RSA_X931_PADDING } = require("constants");
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
   }
-let kezdoKez = []
-kezdoKez.push(parseInt(randomNumber(1,11))+parseInt(randomNumber(1,11)))
-console.log(kezdoKez);
+let kezdoKez
+
+kezdoKez=(parseInt(randomNumber(1,11))+parseInt(randomNumber(1,11)));
+
+
+console.log("hello you have  "+kezdoKez+"  in your hand");
+console.log("do you want to pull more?")
+
+rl.question("y/n      ", function(answer){
+})
