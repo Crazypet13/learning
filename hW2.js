f79fc94f21da701cb23aabc12dd577ce18e27009
CrazyPet <magyar13peter13@gmail.com>
2021-09-24 01:54:54 +0200

    asd

--- hW2.js
+++ hW2.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
 function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
   }
-let kezdoKez = a[]
+let kezdoKez = []
 
 kezdoKez=(parseInt(randomNumber(1,11))+parseInt(randomNumber(1,11)));
 
