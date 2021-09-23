b91a92dab3415d254c1cac0f6a923bce4a9d4f7d
CrazyPet <magyar13peter13@gmail.com>
2021-09-23 22:41:46 +0200

    asd

---
+++ logicGates.js
@@ -0,0 +1,16 @@
+let deadDuck = true;
+let friedDuck = true;
+let goodFood;
+
+goodFood = (deadDuck)||(friedDuck);
+
+let x =true;
+let y =false;
+//!(x&&y);
+//(!x)||(!y);
+console.log(!(x&&y));
+console.log(!(!x)||(!y));
+console.log((!x)&&(!y));
+
+
+
