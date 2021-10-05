e81643470c063f590d463d0a5a2419acad737e7d
CrazyPet <magyar13peter13@gmail.com>
2021-10-06 01:48:27 +0200

    good time with music~~<3

--- gyakorlas2.js
+++ gyakorlas2.js
@@ -87,3 +87,73 @@ evenOrNot("sajto")
 //Christmas Eve is almost upon us, so naturally we need to prepare some milk and cookies for Santa! Create a function that accepts a Date object and returns true if it's Christmas Eve (December 24th) and false otherwise. Keep in mind JavaScript's Date month is 0 based, meaning December is the 11th month while January is 0.
 
 // :/ hmmmmm 
+
+//how the fuck date works-.-
+
+
+//Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+
+function biggerOrNot(number1){
+number1= parseInt(number1);
+   if (number1<=0){
+       return console.log(true);
+   }else{
+        return console.log(false);
+   }
+}
+biggerOrNot(-20)
+
+
+
+//Given an n-sided regular polygon n, return the total sum of internal angles (in degrees).
+
+function szog (many){
+many= parseInt(many); //csak mert ha a fasz string ként addja be tudom hogy fölösleges meg a felsöbe is
+   return console.log((many - 2) * 180+"°");
+}
+szog(5)
+
+//You are counting points for a basketball game, given the amount of 2-pointers scored and 3-pointers scored, find the final points for the team and return that value.
+
+function kosar (twoPoint,threePoint){
+twoPoint = parseInt(twoPoint)
+threePoints = parseInt(threePoint)
+let = score = (0);
+console.log(twoPoint,threePoint)
+score = score+((twoPoint*2)+(threePoints*3));
+return console.log(score);
+}
+kosar(1,1)
+
+
+
+//A student learning JavaScript was trying to make a function. His code should concatenate a passed string name with string "Edabit" and store it in a variable called result. He needs your help to fix this code.
+
+
+/* eredeti code
+function nameString(name){
+	var b = "Edabit"
+	var result == name + b
+  	return result
+}*/
+
+function nameString(name,Edabit){
+let result
+ result = (name+" "+Edabit);
+ return console.log(result);
+}
+
+nameString("fuck","you!");
+
+
+//Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+
+
+function stuff(n1,n2){
+    let n3
+    if (n1<100 && n2 <100) {n3 = true
+    }else n3 = false
+    return console.log(n3)
+}
+stuff(50,60)
+stuff(50,101)
