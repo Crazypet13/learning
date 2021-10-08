93756a7d259a6edabf9502d8b0754937c90e4094
CrazyPet <magyar13peter13@gmail.com>
2021-10-08 23:39:33 +0200

    asdf

--- gyak4.js
+++ gyak4.js
@@ -103,3 +103,29 @@ for 0-tumb.length
 */
 
 
+//van e egy szam ketsze abban a szövegben
+
+let alany= "abadbcde";
+let i=0;
+let j=1;
+
+let megjoE=true;
+let franctudja=true;
+let megoldasIndex=0;
+
+
+for(let...stb...i<alany.length)
+{
+    for (let j=i+1<alany.length)
+    {
+        if (alany[i]==j){
+            megoldasindex = i+1;
+         blabla
+        }else{happyness , mehetek tovabb}
+
+    }
+}
+
+if (megJoE){
+    return megoldas;
+}else throw Exception("Semmisejo");
