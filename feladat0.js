e4d57b189baaee52095ade286aa7a037140ab1b5
CrazyPet <magyar13peter13@gmail.com>
2021-10-12 11:03:29 +0200

    stuff ??

---
+++ feladat0.js
@@ -0,0 +1,8 @@
+// stringböl szed szét szavakra és ha páros/páratlan?? a szó irasd ki arreybe
+//addot string be nézd meg hogy van e ismétlödö rész (betű)
+// perfect szám "1*2*3 = 1+2+3 = 6" az e a beadott szám ?
+// bináris átválto program
+// string fordítos 
+
+//mindegyikbe volt olyan hogy kidbjon hibát ha nem megfelelöt adsz be //pl nem lehet 0 vagy csak pozitive vagy csak betük
+//összesbe kért kicsit konplexebb debuggolos dolgokat hogy jelezzen a program ha rossz van irva vagy valami nem csak hogy lefut azt csö:3
